f3770170149c4521a09e68611466803790ed1d37
0047Adarsh <adarsh9742007@gmail.com>
2024-05-02 17:55:16 +0530

    Final Application

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import ejs from "ejs";
+import ejs, { render } from "ejs";
 import pg from "pg";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -14,15 +14,18 @@ app.set('views', join(__dirname, 'views'));
 app.use(express.static(join(__dirname, 'public')));
 
 var conString = "postgres://xnqqtxls:AyyL6iD3RUYkb53cYXVaMwMKDiR60uVf@floppy.db.elephantsql.com/xnqqtxls"
+//var conString = "postgres://lyzxcret:fVewYdhtTUcY6fDnyFwVMMMnDm0VaH53@floppy.db.elephantsql.com/lyzxcret"
+
 var client = new pg.Client(conString);
 client.connect()
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/volume', (req,res)=>{
     let data = req.body;
     try {
-        client.query('INSERT INTO volume (volume, production_date) VALUES ($1, $2)', [data.volume, data.productiondate]);
+        client.query('INSERT INTO volume (volume, production_date, wastage_volume) VALUES ($1, $2, $3)', [data.volume, data.productiondate, 0.0]);
     } catch (err) {
         console.log(err);
     }
@@ -40,27 +43,93 @@ app.post('/order', async(req,res)=>{
     res.redirect('/');
 });
 
-
-app.get('/', async(req,res)=>{
+app.post('/addcustomer', async(req,res)=>{
+    let data = req.body;
     try {
-        const results = await client.query('SELECT *, delivery_date FROM orders');
-        const vol_results = await client.query('SELECT * FROM volume');
-        res.render('index.ejs', {all_orders: results.rows, f_volume: vol_results.rows});
+        client.query('INSERT INTO customers (customer_name) VALUES ($1)', [data.customer]);
     } catch (err) {
         console.log(err);
     }
+    res.redirect('/');
 });
+
+app.get('/', async (req, res) => {
+    try {
+        const names = await client.query("SELECT customer_name FROM customers");
+        const orders = await client.query('SELECT * FROM orders ORDER BY delivery_date ASC');
+        const volumeResults = await client.query('SELECT * FROM volume ORDER BY production_date ASC');
+        
+        for (const volume of volumeResults.rows) {
+            let ord_vol = 0;
+            let remaining = 0;
+            let wastage = 0;
+            const fullDate = new Date(volume.production_date);
+            const year = fullDate.getFullYear();
+            const month = (fullDate.getMonth() + 1).toString().padStart(2, '0');
+            const day = fullDate.getDate().toString().padStart(2, '0');
+            const previous_day = (day - 1).toString().padStart(2, '0');
+            const prod_date = `${year}-${month}-${day}`;
+            const pprod_date = `${year}-${month}-${previous_day}`;
+            let produced_vol = volume.volume;
+        
+            for (const order of orders.rows) {
+                const orderDate = new Date(order.delivery_date);
+                const orderYear = orderDate.getFullYear();
+                const orderMonth = (orderDate.getMonth() + 1).toString().padStart(2, '0');
+                const orderDay = orderDate.getDate().toString().padStart(2, '0');
+                const ord_date = `${orderYear}-${orderMonth}-${orderDay}`;
+                if (ord_date === prod_date) {
+                    ord_vol += parseFloat(order.volume) * parseFloat(order.quantity);
+                }
+            }
+            for (const vol of volumeResults.rows) {
+                const volDate = new Date(vol.production_date);
+                const volYear = volDate.getFullYear();
+                const volMonth = (volDate.getMonth() + 1).toString().padStart(2, '0');
+                const volDay = volDate.getDate().toString().padStart(2, '0');
+                const nprod_date = `${volYear}-${volMonth}-${volDay}`;
+                if (nprod_date === pprod_date) {
+                    remaining += parseFloat(vol.remaining_volume);
+                    wastage = parseFloat(vol.wastage_volume);
+                }
+            }
+            let total_vol = parseFloat(produced_vol) + parseFloat(remaining) - parseFloat(wastage);
+            let p_remaining = parseFloat(total_vol) - parseFloat(ord_vol);
+            
+            await client.query('UPDATE volume SET order_volume = $1, total_volume = $2, remaining_volume = $3 WHERE production_date = $4', [ord_vol, total_vol, p_remaining, prod_date]);
+        }
+        
+        const ord_data = await client.query('SELECT * FROM orders');
+        const vol_data = await client.query('SELECT * FROM volume ORDER BY production_date DESC');
+        
+        res.render('index.ejs', { all_orders: ord_data.rows, f_volume: vol_data.rows, customer_names: names.rows });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 app.get('/dashboard', async(req,res)=>{
+    const date = new Date();
     try {
-        const results = await client.query('SELECT *, delivery_date FROM orders');
-        const vol_results = await client.query('SELECT * FROM volume');
-        res.render('dashboard.ejs', {all_orders: results.rows, f_volume: vol_results.rows});
+        const results = await client.query('SELECT * FROM orders WHERE delivery_date = $1',[date]);
+        res.render('dashboard.ejs', {all_orders: results.rows});
     } catch (err) {
         console.log(err);
     }
 });
 
+app.post('/filtered', async (req, res) => {
+    const selectedDate = req.body.selected_date;
+    try {
+        const results = await client.query('SELECT * FROM orders WHERE delivery_date = $1',[selectedDate]);
+        res.json(results.rows);
+    } catch (error) {
+        console.error('Error fetching filtered orders:', error);
+        res.status(500).json({ error: 'Error fetching filtered orders' });
+    }
+});
 
 app.post('/update_order', (req, res) => {
     let data = req.body;
@@ -82,6 +151,18 @@ app.post('/update_volume', (req,res)=>{
     let udata = req.body;
     client.query('UPDATE volume SET volume = $1, production_date = $2 WHERE slno = $3',[udata.uvvolume,udata.uvproductiondate,udata.uvSl]);
     res.redirect("/");
+});
+
+app.post('/update_wastage', (req,res)=>{
+    let data = req.body;
+    client.query('UPDATE volume SET wastage_volume = $1 WHERE slno = $2',[data.wastage,data.slno]);
+    res.redirect("/");
+});
+
+app.post('/update_order_status', async(req,res)=>{
+    const { orderId, type } = req.body;
+    client.query('UPDATE orders SET order_status=$1 WHERE sequence=$2',[type, orderId])
+    res.redirect('/dashboard');
 });
 
 app.listen(`${port}`, ()=>{
